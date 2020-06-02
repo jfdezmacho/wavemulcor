ea5bfa4146b981db9da9930061b07263df7f6bca
jfdezmacho <javier.fernandezmacho@gmail.com>
2020-06-02 17:24:51 +0200

    heatmap_wlmcc: common clim all plots

--- R/heatmap_wave.local.multiple.cross.correlation.R
+++ R/heatmap_wave.local.multiple.cross.correlation.R
@@ -14,6 +14,9 @@ heatmap_wave.local.multiple.cross.correlation <- #3.1.1
     level.labs <- c(paste("level",1:J),paste("s",J))
     scale.labs <- c(paste0(2^(1:J),"-",2^((1:J)+1)),"smooth")
     lag.labs <- c(paste("lead",lag.max:1),paste("lag",0:lag.max))
+    if (is.null(ci)||ci=="center") {clim <- range(sapply(cor,function(x){x[["vals"]]}))
+    } else if (ci=="lower") {clim <- range(sapply(cor,function(x){x[["lower"]]}))
+    } else if (ci=="upper"){clim <- range(sapply(cor,function(x){x[["upper"]]}))}
     mark <- paste0("\u00A9jfm-wavemulcor3.1.0_",Sys.time()," ")
     if(lag.first){
       par(mfcol=c(lmax+1,2), las=1, pty="m", mar=c(2,3,1,0)+.1, oma=c(0.1,1.2,1.2,1.2))
@@ -25,11 +28,11 @@ heatmap_wave.local.multiple.cross.correlation <- #3.1.1
         } else if (ci=="lower") {val <- sapply(cor,function(x){x[["lower"]][,i]})
         } else if (ci=="upper"){val <- sapply(cor,function(x){x[["upper"]][,i]})}
         colkey <- FALSE
-        if (i==lag0) colkey=list(cex.axis=0.75)
+        if (i==lag0) colkey <- list(cex.axis=0.75)
         plot3D::image2D(z=val, x=1:nrow(val), y=1:ncol(val),
                         main=lag.labs[i], sub="", xlab="", ylab="",
                         xaxt=xaxt, yaxt="n", cex.axis=0.75,
-                        colkey=colkey, clab = expression(varphi),
+                        colkey=colkey, clim=clim, clab=expression(varphi),
                         rasterImage=T, contour=list(lwd=2, col=plot3D::jet.col(11)))
         text(x=grconvertX(0.1,from="npc"), y=grconvertY(0.97,from="npc"), labels=mark,
              col=rgb(0,0,0,.1),cex=.2)
@@ -51,11 +54,11 @@ heatmap_wave.local.multiple.cross.correlation <- #3.1.1
         } else if (ci=="lower") {val <- cor[[j]]$lower[,lag0+(-lmax:lmax)]
         } else if (ci=="upper"){val <- cor[[j]]$upper[,lag0+(-lmax:lmax)]}
         colkey <- FALSE
-        if (j==1) colkey=list(cex.axis=0.75)
+        if (j==1) colkey <- list(cex.axis=0.75)
         plot3D::image2D(z=val, x=1:nrow(val), y=1:ncol(val),
                         main=level.labs[j], sub="", xlab="", ylab="",
                         xaxt=xaxt, yaxt="n", cex.axis=0.75,
-                        colkey=colkey, clab = expression(varphi),
+                        colkey=colkey, clim=clim, clab=expression(varphi),
                         rasterImage=T, contour=list(lwd=2, col=plot3D::jet.col(11)))
         text(x=grconvertX(0.1,from="npc"), y=grconvertY(0.97,from="npc"), labels=mark,
              col=rgb(0,0,0,.15),cex=.2)
@@ -63,7 +66,7 @@ heatmap_wave.local.multiple.cross.correlation <- #3.1.1
         if(xaxt!="s") {axis(side=1, at=at, labels=label, cex.axis=0.75)}
         axis(side=2, at = if((J%%2==0&j%%2==1)||(J%%2==1&j%%2==0)) 1:ncol(val) else FALSE,
              labels = if((J%%2==0&j%%2==1)||(J%%2==1&j%%2==0)) lag.labs[lag0+(-lmax:lmax)] 
-                      else FALSE, las=1, cex.axis=0.75)
+             else FALSE, las=1, cex.axis=0.75)
       }
       title(main='Wavelet Local Multiple Cross-Correlation', outer=TRUE)
       mtext("lead / lag", side=2, outer=TRUE, adj=0.5)
